7b8a2097deb1c9c834930ea0be80e5a630ba82fc
Somoye Ayotunde <somoye.ayotunde@gmail.com>
2019-10-23 09:10:15 +0100

    Add recipes list

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Search from "../components/Search";
+import RecipeList from "../components/RecipeList";
+import { recipeData } from "../data/tempList";
 
 export default class Recipes extends Component {
-    render() {
-        return (
-            <h4>
-                Hello from Recipe
-            </h4>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    recipes: recipeData,
+    search: ""
+  };
+  handleChange = e => {
+    this.setState({ search: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <>
+        <Search
+          search={this.state.search}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <RecipeList recipes={this.state.recipes} />
+      </>
+    );
+  }
 }
