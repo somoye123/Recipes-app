b32b006fdcebc2eeab6fce09c441aa6cd605702d
Somoye Ayotunde <somoye.ayotunde@gmail.com>
2019-10-25 13:16:38 +0100

    update recipes list in recipes page

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import Search from "../components/Search";
 import RecipeList from "../components/RecipeList";
-import { recipeData } from "../data/tempList";
 
 export default class Recipes extends Component {
   constructor(props) {
     super(props);
+    this.getRecipes = this.getRecipes.bind(this);
   }
   state = {
-    recipes: recipeData,
-    search: ""
+    recipes: [],
+    search: "",
+    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
   };
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentDidMount() {
+    this.getRecipes();
+  }
   handleChange = e => {
     this.setState({ search: e.target.value });
   };
